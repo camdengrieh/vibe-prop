14f93bea181f8f17f779108ba691037630d7ca68
Camden <82294478+camdengrieh@users.noreply.github.com>
2024-12-30 13:46:47 +0000

    Update Header.tsx

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useCallback, useRef, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
